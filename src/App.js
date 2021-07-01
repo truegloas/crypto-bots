b62ad998bd488b8f2fccab0aae3b4f9028240c44
truegloas <gloasrow@gmail.com>
2021-07-01 16:45:54 +0700

    First try to make GET request

--- src/App.js
+++ src/App.js
@@ -1,25 +1,45 @@
+import React, { Component } from 'react'
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://back.yourtar.ru/api/admin/bot/?offset=0"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Get Bots
-        </a>
-      </header>
-    </div>
-  );
+
+const BotList = ({bots}) => {
+  bots.map(bot => <li key={bot}>{bot}</li>)
+}
+
+const BOT_SERVICE_URL = 'https://back.yourtar.ru/api/admin/bot/?offset=0'
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.timer = null;
+    this.state = {
+      isFetching: false,
+      bots: ['1']};
+  }
+
+  render () {
+    return (
+        <div className="App">
+          <header className="App-header">
+            <img src={logo} className="App-logo" alt="logo"/>
+            <BotList bots={this.state.bots} />
+          </header>
+        </div>
+    );
+  }
+
+  componentDidMount() {
+    this.fetchBots()
+  }
+
+  fetchBots = () => {
+    this.setState({...this.state, isFetching: true})
+    fetch(BOT_SERVICE_URL)
+        .then(response => response.json())
+        .then(result => this.setState({bots: result, isFetching: false}))
+        .catch(e => console.log(e));
+  }
 }
 
 export default App;
