ea34cbc0ac64a73e7903ac43e3063cf23bf8823d
truegloas <gloasrow@gmail.com>
2021-07-04 19:27:29 +0700

    Add buttons to manage bots

--- src/CryptoBots.js
+++ src/CryptoBots.js
@@ -1,25 +1,56 @@
 import axios from 'axios'
 import React, { useState } from 'react'
-import { Container, Row, Button } from 'react-bootstrap'
+import {Container, Row, Button, Col} from 'react-bootstrap'
 
 import './CryptoBots.css';
 import TOKEN from './local/local.js';
 
 
 function CryptoBots() {
-    const BOT_SERVICE_URL = 'https://back.yourtar.ru/api/admin/bot/?offset=0'
+    const BOT_URL_SERVICE = 'https://back.yourtar.ru/api/admin/bot/';
+
+    const config = {headers: {
+            "YT-AUTH-TOKEN": TOKEN
+        }}
 
     const [bots, setBots] = useState(null);
 
     const getBots = async () => {
         const response = await axios.get(
-            BOT_SERVICE_URL,
-            {headers: {
-                "YT-AUTH-TOKEN": TOKEN
-            }});
-        console.log(response.data['data'].id)
-        setBots(response.data['data']);
+            'https://back.yourtar.ru/api/admin/bot/?offset=0',
+            config
+        );
+        setBots(response.data['data'])
     };
+
+    const addBot = async () => {
+        const response = await axios.post(
+            BOT_URL_SERVICE,
+            {"user": "1"},
+            {
+                headers: {
+                    "YT-AUTH-TOKEN": TOKEN
+                },
+                data: {
+                    "user": "1"
+                }
+            }
+        );
+    };
+
+    const deleteBotById = async (bot_id) => {
+        const response = await axios.delete(
+            BOT_URL_SERVICE,
+            {
+                headers: {
+                    "YT-AUTH-TOKEN": TOKEN
+                },
+                data: {
+                    "id": bot_id
+                }
+            }
+        )
+    }
 
     return (
         <Container className="CryptoBots">
@@ -28,24 +59,34 @@ function CryptoBots() {
                 <h2>Manage data with the buttons below!</h2>
             </Row>
 
-            <Row>
-                <Button className="fetch-Button" onClick={getBots}>
+            <Row className='justify-content-around fixed-top'>
+                <Button onClick={getBots}>
                     Get Bots
+                </Button>
+                <Button onClick={addBot}>
+                    Add Bot
                 </Button>
             </Row>
 
-            <Row className="flex-column justify-content-center align-items-center">
+            <Row className="flex-column justify-content-center align-items-center text-nowrap">
                 {bots && bots.map((bot) => {
                     return (
-                        <Row className="" key={bot.id}>
-                            <Row className="">
-                                <p>Идентификатор бота: {bot.id}</p>
-                                <p>Пользователи бота: {bot.users.map((user) => {
+                        <Row key={bot.id}>
+                            <Col className='flex-row flex-nowrap'>
+                                <p>Идентификатор бота: </p>
+                                <p>{bot.id}</p>
+                            </Col>
+                            <Col className='flex-row flex-nowrap col-auto'>
+                                <p>Пользователи бота: </p>
+                                {bot.users.map((user) => {
                                     return (
-                                        <p>{user}</p>
+                                        <p>{user}, </p>
                                     )
-                                })}</p>
-                            </Row>
+                                })}
+                            </Col>
+                            <Button onClick={deleteBotById(bot.id)}>
+                                Удалить бота
+                            </Button>
                         </Row>
                     );
                 })}
